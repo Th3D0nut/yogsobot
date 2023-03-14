703684aafe1b7f9901f5df48b7abe8de0ee806e7
Roan <roanpaulus@outlook.com>
2023-03-14 19:22:58 +0100

    improved test with fixtures

--- tests/database/test_init_tables.py
+++ tests/database/test_init_tables.py
@@ -9,22 +9,23 @@ from yogsobot.database.utills import init_tables
 
 DIR = dirname(__file__)
 PATH_TO_DB = join(DIR, "test.db")
-db_connection = sqlite3.connect(PATH_TO_DB)
-db_curs = db_connection.cursor()
 
-def test_add_tables_when_tables_not_present():
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    # Setup
+    db_connection = sqlite3.connect(PATH_TO_DB)
+    db_curs = db_connection.cursor()
+    yield db_curs
+    # Teardown
+    db_curs.close()
+    db_connection.close()
+    testdb = join(DIR, "test.db")
+    os.remove(testdb)
+
+
+def test_add_tables_when_tables_not_present(run_around_tests):
+    db_curs = run_around_tests
     init_tables(db_curs)
     res = db_curs.execute("SELECT name FROM sqlite_master")
     assert res.fetchone() is not None
-    db_curs.close()
-    db_connection.close()
-    try:
-        testdb = join(DIR, "test.db")
-        os.remove(testdb)
-    except OSError as e:
-        raise OSError(f"File: {e.filename} not found, therefore can't remove. {e.strerror}")
-
-
-def test_does_not_add_tables_when_present():
-    # TODO: implement
-    pass
