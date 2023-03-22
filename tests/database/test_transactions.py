92ec5e7ca3e96fc53025ff97d4c6bbe2814d35a5
Roan <roanpaulus@outlook.com>
2023-03-22 19:57:09 +0100

    added method to for saving rolls

--- tests/database/test_transactions.py
+++ tests/database/test_transactions.py
@@ -22,6 +22,7 @@ def db():
     os.remove(testdb)
 
 
+# init_tables tests
 def test_init_tables_creates_user_and_roll_alias_tables(db):
     db.init_tables()
 
@@ -32,9 +33,22 @@ def test_init_tables_creates_user_and_roll_alias_tables(db):
     assert len(table_names.fetchall()) == 2
 
 
+# save_user tests
 def test_does_not_save_user_that_already_exist(db):
     db.init_tables()
     db.save_user(discord_id="12345678")
 
     with pytest.raises(sqlite3.IntegrityError):
         db.save_user(discord_id="12345678")
+
+
+# tests
+def test_find_roll_in_db_after_saving_it(db):
+    db.init_tables()
+    db.save_user(discord_id="12345678")
+    db.save_roll(discord_id="12345678", alias="fireball", roll_expression="2d8 d10")
+
+    discord_id = db.cursor.execute("""SELECT user_id, alias FROM roll_alias
+        WHERE user_id = 12345678 AND alias = 'fireball';
+    """)
+    assert discord_id.fetchone() == ("12345678", "fireball")
--- yogsobot/database/transactions.py
+++ yogsobot/database/transactions.py
@@ -2,29 +2,37 @@ import sqlite3
 
 
 class DatabaseActor:
-    def __init__(self, path_to_db):
+    def __init__(self, path_to_db: str):
         self.connection = sqlite3.connect(path_to_db)
         self.cursor = self.connection.cursor()
 
     def init_tables(self):
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS user(
-                id INTEGER PRIMARY KEY,
                 discord_id TEXT NOT NULL UNIQUE
                 );
-            """)
+            """)  # discord_id replaces the need for a primary key
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS roll_alias(
                 id INTEGER PRIMARY KEY,
-                user_id INTEGER NOT NULL,
-                name TEXT NOT NULL,
-                roll TEXT
+                user_id TEXT NOT NULL,
+                alias TEXT NOT NULL,
+                roll_expression TEXT
                 );
             """)
 
-    def save_user(self, discord_id):
+    def save_user(self, discord_id: str):
         self.cursor.execute(
             "INSERT INTO user (discord_id) VALUES (?);",
             (discord_id,)
+            )
+        self.connection.commit()
+
+    def save_roll(self, discord_id: str, alias: str, roll_expression: str):
+        self.cursor.execute("""
+            INSERT INTO roll_alias (user_id, alias, roll_expression)
+                VALUES (?, ?, ?);
+            """,
+            (discord_id, alias, roll_expression)
             )
         self.connection.commit()
